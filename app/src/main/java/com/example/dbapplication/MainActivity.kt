d36b798261715851c40629e433bee3149bfcda6d
Arun <aarunpdy@gmail.com>
2022-04-12 15:04:28 +0530

    Android Project added

---
+++ app/src/main/java/com/example/dbapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dbapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
